57af1422b5b653cdb7361062566d4e8e4faa817c
Mirkataaa <156722755+Mirkataaa@users.noreply.github.com>
2025-10-27 22:12:07 +0200

    feat(auth): add signIn endpoint

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,15 +1,22 @@
 import { Body, Controller, Post, UsePipes } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ZodValidationPipe } from 'src/common/pipes/zod-validation.pipe';
-import { type CreateUserDto, createUserSchema } from './user.schema';
+import { type UserDto, UserSchema } from './user.schema';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('/register')
-  @UsePipes(new ZodValidationPipe(createUserSchema))
-  async register(@Body() authCredentialsDto: CreateUserDto): Promise<void> {
+  @Post('register')
+  @UsePipes(new ZodValidationPipe(UserSchema))
+  async register(@Body() authCredentialsDto: UserDto): Promise<void> {
     await this.authService.register(authCredentialsDto);
+  }
+
+  @Post('/signin')
+  async signIn(
+    @Body() authCredentialsDto: UserDto,
+  ): Promise<{ accessToken: string }> {
+    return await this.authService.signIn(authCredentialsDto);
   }
 }
