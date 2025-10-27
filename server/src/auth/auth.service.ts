cc15ef8a29359d303edf27587b96e02ff70c716f
Mirkataaa <156722755+Mirkataaa@users.noreply.github.com>
2025-10-27 22:11:55 +0200

    feat(auth): implement signIn method

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -2,21 +2,25 @@ import {
   ConflictException,
   Injectable,
   InternalServerErrorException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
-import { CreateUserDto } from './user.schema';
+import { UserDto } from './user.schema';
 import * as bcrypt from 'bcrypt';
 import { isPostgresError } from 'src/common/utils/postgres-error.util';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User) private usersRepositoy: Repository<User>,
+    private jwtService: JwtService,
   ) {}
 
-  async register(authCredentialsDto: CreateUserDto): Promise<void> {
+  async register(authCredentialsDto: UserDto): Promise<void> {
     const { email, username, password, role } = authCredentialsDto;
 
     const salt = await bcrypt.genSalt();
@@ -37,6 +41,21 @@ export class AuthService {
       }
 
       throw new InternalServerErrorException();
+    }
+  }
+
+  async signIn(authCredentialsDto: UserDto): Promise<{ accessToken: string }> {
+    const { email, password, username } = authCredentialsDto;
+    const user = await this.usersRepositoy.findOne({
+      where: { email },
+    });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const payload: JwtPayload = { email, username };
+      const accessToken = this.jwtService.sign(payload);
+      return { accessToken };
+    } else {
+      throw new UnauthorizedException('Wrong Credentials!');
     }
   }
 }
