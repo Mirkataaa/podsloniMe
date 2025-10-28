15994abc8ec0508469467c58f680fe98abb618b2
Mirkataaa <156722755+Mirkataaa@users.noreply.github.com>
2025-10-28 16:29:00 +0200

    feat(users): add UsersService with create/find methods

---
+++ server/src/users/users.service.ts
@@ -0,0 +1,39 @@
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './user.schema';
+import { parsePostgresUniqueError } from 'src/common/utils/parse-postgres-unique-error.util';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepo: Repository<User>,
+  ) {}
+
+  async createUser(
+    createUserDto: Partial<User> | CreateUserDto,
+  ): Promise<User> {
+    const user = this.userRepo.create(createUserDto);
+
+    try {
+      return await this.userRepo.save(user);
+    } catch (error) {
+      const message = parsePostgresUniqueError(error);
+      if (message) throw new ConflictException(message);
+
+      throw new InternalServerErrorException(
+        'Грешка при създаване на потребител',
+      );
+    }
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.userRepo.findOne({ where: { email } });
+  }
+}
