8c4ece384666ea607f6012e808746b24ff90a56c
Mirkataaa <156722755+Mirkataaa@users.noreply.github.com>
2025-10-31 09:17:39 +0200

    feat(agencies): add Agency entity with members relation

---
+++ server/src/agencies/agency.entity.ts
@@ -0,0 +1,45 @@
+import { User } from 'src/users/user.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('agencies')
+export class Agency {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ nullable: true })
+  description?: string;
+
+  @Column({ nullable: true })
+  logo?: string;
+
+  @Column({ nullable: true })
+  phone?: string;
+
+  @Column({ nullable: true })
+  email?: string;
+
+  @Column({ default: false })
+  isApproved: boolean;
+
+  @Column({ type: 'decimal', precision: 5, scale: 2, default: 15 })
+  commissionCut: number;
+
+  @OneToMany(() => User, (user) => user.agency)
+  members: User[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
