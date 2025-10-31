09352c57c4cc5259bbd3af842cfb0bc935072e9a
Mirkataaa <156722755+Mirkataaa@users.noreply.github.com>
2025-10-31 11:03:01 +0200

    feat(auth): implement Roles decorator and RolesGuard for role-based access control

---
+++ server/src/common/guards/roles.guard.ts
@@ -0,0 +1,36 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { UserRole } from 'src/users/user.entity';
+import { RequestWithUser } from '../interfaces/request-with-user.interface';
+
+export class RolesGuards implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<UserRole[]>(
+      'roles',
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles || requiredRoles.length === 0) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest<RequestWithUser>();
+    const user = request.user;
+
+    if (!user) {
+      throw new ForbiddenException('Missing user context');
+    }
+
+    if (!requiredRoles.includes(user.role)) {
+      throw new ForbiddenException('Нямате достъп до този ресурс');
+    }
+
+    return true;
+  }
+}
