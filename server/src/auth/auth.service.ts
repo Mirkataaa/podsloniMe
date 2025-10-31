27c1a0281d31881ffd6dfab7a067e4d572c1f551
Mirkataaa <156722755+Mirkataaa@users.noreply.github.com>
2025-10-31 09:55:52 +0200

    feat(auth): extend register logic to handle brokers and agency creation

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,31 +1,80 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { CreateUserDto, LoginUserDto } from '../users/user.schema';
+import {
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { CreateUserDto, LoginUserDto } from '../users/user.dto';
 import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 import { JwtPayload } from './jwt-payload.interface';
 import { UsersService } from 'src/users/users.service';
+import { UserRole } from 'src/users/user.entity';
+import { AgenciesService } from 'src/agencies/agencies.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly usersService: UsersService,
     private readonly jwtService: JwtService,
+    private readonly agenciesService: AgenciesService,
   ) {}
 
   async register(authCredentialsDto: CreateUserDto): Promise<void> {
-    const { email, username, password, role } = authCredentialsDto;
+    const {
+      email,
+      username,
+      password,
+      role,
+      agencyId,
+      agencyName,
+      createNewAgency,
+    } = authCredentialsDto;
 
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    const user = {
+    const baseUser = {
       email,
       username,
       password: hashedPassword,
       role,
     };
 
-    await this.usersService.createUser(user);
+    try {
+      if (role === UserRole.USER) {
+        await this.usersService.createUser({
+          ...baseUser,
+          isApproved: true,
+        });
+
+        return;
+      }
+
+      if (role === UserRole.BROKER) {
+        let agency = null;
+
+        if (createNewAgency && agencyName) {
+          agency = await this.agenciesService.createAgency({
+            name: agencyName,
+            isApproved: false,
+          });
+        } else if (agencyId) {
+          agency = await this.agenciesService.findOne(agencyId);
+        }
+
+        await this.usersService.createUser({
+          ...baseUser,
+          agency,
+          isApproved: false,
+        });
+
+        return;
+      }
+    } catch (error) {
+      console.log('Registration failed:', error);
+
+      throw new InternalServerErrorException('Грешка при регистрация');
+    }
   }
 
   async signIn(
