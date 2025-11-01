b866a1bd7769cb2cf3c43f0088801d799b65fcd7
Mirkataaa <156722755+Mirkataaa@users.noreply.github.com>
2025-11-01 11:03:55 +0200

    feat(user): add update and delete user methods to UserService

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -2,6 +2,7 @@ import {
   ConflictException,
   Injectable,
   InternalServerErrorException,
+  NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
@@ -33,11 +34,32 @@ export class UsersService {
     }
   }
 
+  async findById(id: string): Promise<User | null> {
+    return this.userRepo.findOne({ where: { id } });
+  }
+
   async findByEmail(email: string): Promise<User | null> {
     return this.userRepo.findOne({ where: { email } });
   }
 
   async save(user: User): Promise<User> {
     return this.userRepo.save(user);
+  }
+
+  async update(id: string, data: Partial<User>): Promise<User> {
+    const user = await this.findById(id);
+    if (!user) throw new NotFoundException('User not found');
+    Object.assign(user, data);
+    return await this.userRepo.save(user);
+  }
+
+  async deleteUser(id: string): Promise<void> {
+    const user = await this.findById(id);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    await this.userRepo.delete(id);
   }
 }
