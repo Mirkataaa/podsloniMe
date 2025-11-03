b330dcb5b09aad93de10894267b6cf1ba611d056
Mirkataaa <156722755+Mirkataaa@users.noreply.github.com>
2025-11-03 21:12:03 +0200

    feat(auth): add auth zustand store with persist and role helpers

---
+++ client/src/app/features/auth/store/auth.store.ts
@@ -0,0 +1,27 @@
+import { create } from 'zustand';
+import type { User } from '../../../shared/types/user.types';
+import { devtools, persist } from 'zustand/middleware';
+import type { AuthState } from '../../../shared/types/auth.types';
+
+export const useAuthStore = create<AuthState>()(
+  devtools(
+    persist(
+      (set, get) => ({
+        token: null,
+        user: null,
+
+        setAuth: (token: string, user: User) => {
+          set({ token, user });
+        },
+
+        logout: () => {
+          set({ token: null, user: null });
+        },
+
+        isAdmin: () => get().user?.role === 'admin',
+        isBroker: () => get().user?.role === 'broker',
+      }),
+      { name: 'auth' },
+    ),
+  ),
+);
