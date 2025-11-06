ff69727ff4a231b4eb582745e6f6d544e3c695ff
Mirkataaa <156722755+Mirkataaa@users.noreply.github.com>
2025-11-06 15:31:05 +0200

    refactor(ui): add top spacing to hero section

--- client/src/app/pages/Home/components/HeroSection.tsx
+++ client/src/app/pages/Home/components/HeroSection.tsx
@@ -1,12 +1,12 @@
 export default function HeroSection() {
   return (
     <>
-      <section className="bg-linear-to-br from-blue-50 to-green-50 py-24 text-center relative overflow-hidden">
+      <section className="bg-linear-to-br from-blue-50 to-green-50 py-24 text-center relative overflow-hidden mt-5">
         <div className="absolute inset-0 bg-[url('https://images.unsplash.com/photo-1600585154340-be6161a56a0c?auto=format&fit=crop&w=1400&q=80')] bg-cover bg-center opacity-10"></div>
 
         <div className="relative z-10 container mx-auto px-6">
           <h1 className="text-4xl md:text-6xl font-bold text-gray-800 mb-4">
-            Намери място, което да наречеш{" "}
+            Намери място, което да наречеш{' '}
             <span className="text-blue-600">дом</span>.
           </h1>
           <p className="text-xl text-gray-600 mb-8">
