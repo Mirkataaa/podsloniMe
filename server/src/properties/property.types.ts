e755a7d289bfbaf01b53aa7852b8b9283e7816d1
Mirkataaa <156722755+Mirkataaa@users.noreply.github.com>
2025-11-26 13:29:30 +0200

    feat(properties): add core property enums and types

---
+++ server/src/properties/property.types.ts
@@ -0,0 +1,44 @@
+export enum Currency {
+  BGN = 'BGN',
+  EUR = 'EUR',
+}
+
+export enum TransactionType {
+  SALE = 'sale',
+  RENT = 'rent',
+}
+
+export enum PropertyType {
+  APARTMENT = 'apartment',
+  HOUSE = 'house',
+  OFFICE = 'office',
+  LAND = 'land',
+  GARAGE = 'garage',
+  OTHER = 'other',
+}
+
+export enum ConstructionType {
+  BRICK = 'brick',
+  PANEL = 'panel',
+  EPK = 'epk',
+  NEW_BUILD = 'new_build',
+  MONOLITHIC = 'monolithic',
+  OTHER = 'other',
+}
+
+export enum HeatingType {
+  NONE = 'none',
+  ELECTRIC = 'electric',
+  GAS = 'gas',
+  TEC = 'tec',
+  AIRCON = 'aircon',
+  PELLET = 'pellet',
+  OTHER = 'other',
+}
+
+export enum BuildingStage {
+  SHELL = 'shell',
+  ACT14 = 'act14',
+  ACT15 = 'act15',
+  ACT16 = 'act16',
+}
