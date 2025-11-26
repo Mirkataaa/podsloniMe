3fe5606f4e5cd0a018fc8edf11a1e9df3a00407d
Mirkataaa <156722755+Mirkataaa@users.noreply.github.com>
2025-11-26 14:41:18 +0200

    feat(properties): add findAll method with filtering and sorting

--- server/src/properties/properties.service.ts
+++ server/src/properties/properties.service.ts
@@ -3,7 +3,9 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Property } from './property.entity';
 import { Repository } from 'typeorm';
 import { CreatePropertyDto, UpdatePropertyDto } from './property.dto';
+import { User } from 'src/users/user.entity';
 import { toPropertyEntity } from './property.mapper';
+
 @Injectable()
 export class PropertiesService {
   constructor(
@@ -16,5 +18,38 @@ export class PropertiesService {
     const entity = this.propertiesRepo.create(entityInput);
 
     return await this.propertiesRepo.save(entity);
+  }
+
+  async findAll(params?: {
+    settlement?: string;
+    transactionType?: string;
+    propertyType?: string;
+    minPrice?: number;
+    maxPrice?: number;
+  }): Promise<Property[]> {
+    const qb = this.propertiesRepo.createQueryBuilder('p');
+
+    qb.leftJoinAndSelect('p.images', 'images');
+
+    if (params?.settlement) {
+      qb.andWhere('p.settlement ILIKE :settlement', {
+        settlement: `%${params.settlement}%`,
+      });
+    }
+    if (params?.transactionType) {
+      qb.andWhere('p.transactionType = :tt', { tt: params.transactionType });
+    }
+    if (params?.propertyType) {
+      qb.andWhere('p.propertyType = :pt', { pt: params.propertyType });
+    }
+    if (params?.minPrice) {
+      qb.andWhere('p.price >= :min', { min: params.minPrice });
+    }
+    if (params?.maxPrice) {
+      qb.andWhere('p.price <= :max', { max: params.maxPrice });
+    }
+
+    qb.orderBy('p.createdAt', 'DESC');
+    return qb.getMany();
   }
 
