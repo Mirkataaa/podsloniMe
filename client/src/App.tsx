b52ac342bbe8b9a0f2811af4882058230e9f01b7
Mirkataaa <156722755+Mirkataaa@users.noreply.github.com>
2025-12-05 19:49:54 +0200

    feat(core): setup TanStack Query provider

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,22 @@
+import 'leaflet/dist/leaflet.css';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import HomePage from './pages/Home/HomePage';
-import AppLayout from './AppLayout';
-import { AdminGuard } from './shared/guards/AdminGuard';
-import AdminDashboardLayout from './pages/admin/AdminDashBoard';
+import HomePage from './app/pages/Home/HomePage';
+import AppLayout from './app/AppLayout';
+import { AdminGuard } from './app/shared/guards/AdminGuard';
+import AdminDashboardLayout from './app/pages/admin/AdminDashBoard';
+import AddProperty from './app/features/properties/pages/AddProperty';
+import PropertiesList from './app/features/properties/pages/PropertiesList';
+import PropertyDetailsPage from './app/features/properties/pages/PropertyDetailsPage';
 
 function App() {
   return (
     <Routes>
       <Route element={<AppLayout />}>
         <Route path="/" element={<HomePage />} />
+        <Route path="/add-property" element={<AddProperty />} />
+        <Route path="/properties" element={<PropertiesList />} />
+        <Route path="/properties/:id" element={<PropertyDetailsPage />} />
       </Route>
 
       <Route element={<AdminGuard />}>
